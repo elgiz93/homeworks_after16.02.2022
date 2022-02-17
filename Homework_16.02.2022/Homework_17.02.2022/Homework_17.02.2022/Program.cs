d5dc27fb7b758042658d9b905598767892acefe8
Elgiz Aliyev <elgizaa@code.edu.az>
2022-02-17 17:46:13 +0400

    third

---
+++ Homework_16.02.2022/Homework_17.02.2022/Homework_17.02.2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_17._02._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
