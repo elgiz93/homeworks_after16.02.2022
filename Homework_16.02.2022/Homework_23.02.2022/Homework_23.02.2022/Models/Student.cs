5af30b6ffeeab1d73b3ebbcf1490c7070d692384
Elgiz Aliyev <elgizaa@code.edu.az>
2022-02-24 06:45:33 +0400

    six

---
+++ Homework_16.02.2022/Homework_23.02.2022/Homework_23.02.2022/Models/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_23._02._2022.Models
+{
+    internal class Student
+    {
+        public String FullName;
+        public String GroupNo;
+        public byte Age;
+
+        public Student(string fullname,string groupno,byte age)
+        {
+            while (!CheckFullname(fullname))
+            { Console.WriteLine("FullName duzgun daxil edin"); 
+                fullname = Console.ReadLine(); }
+
+                 FullName = fullname;
+
+            while  (!CheckGroupNo(groupno))
+            { Console.WriteLine("GroupNo duzgun daxil edin");
+                groupno = Console.ReadLine(); }
+          
+            GroupNo=groupno;
+
+            Age = age;
+        }
+
+
+        static bool CheckGroupNo(string nomre)
+        {
+            if (nomre.Length == 4 && char.IsUpper(nomre[0]) && char.IsDigit(nomre[1])
+                    && char.IsDigit(nomre[2]) && char.IsDigit(nomre[3]))
+            { 
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+        static bool CheckFullname(string FullName)
+        {    
+            
+            if (FullName.IndexOf(" ")>2 && FullName.IndexOf(" ") < (FullName.Length-2)) ;
+            {
+                if (FullName.IndexOf(" ", (FullName.IndexOf(" ") + 1)) == -1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Homework_16.02.2022/Homework_23.02.2022/Homework_23.02.2022/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Homework_23._02._2022.Models;
+
+namespace Homework_23._02._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FullName daxil edin");
+            string fullname = Console.ReadLine();
+            Console.WriteLine("GroupNo daxil edin");
+            string groupno = Console.ReadLine();
+            Console.WriteLine("Age daxil edin");
+            byte age = byte.Parse(Console.ReadLine());
+            Student student = new Student(fullname, groupno, age);
+            Console.WriteLine("Student elave edildi!");
+            Console.WriteLine($"{student.FullName},{student.GroupNo},{student.Age}");
+        }
+    }
+}
